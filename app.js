b5a5ee4001ae851b18873b8c8f46b559e3353b03
ごみっきぃ <168516977+gmcky139@users.noreply.github.com>
2026-03-17 23:25:56 +0900

    update

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const DATA_URL = 'https://raw.githubusercontent.com/gmcky139/movie-schedule-api/refs/heads/main/movies.json';
 
 async function fetchMovies(){
+    const statusMessageEl = document.getElementById('status_message');
+    const cinemasContainerEl = document.getElementById('cinemas_container');
+
     try{
-        const response = await fetch(DATA_URL);
+        const response = await fetch(DATA_URL + "?t=" + new Date().getTime());
         const data = await response.json();
 
-        const movieNameEl = document.getElementById('cinema_name');
-        const movieListEl = document.getElementById('movie_list');
+        // 読み込み中の文字を消して、コンテナを空にする
+        statusMessageEl.style.display = "none";
+        cinemasContainerEl.innerHTML = "";
 
-        movieNameEl.textContent = `${data.cinema_name}`;
+        data.forEach(cinema => {
+            const cinemaTitle = document.createElement('h2');
+            cinemaTitle.className = 'cinema_title';
+            cinemaTitle.textContent = `${cinema.cinema_name}`;
+            cinemasContainerEl.appendChild(cinemaTitle);
 
-        movieListEl.innerHTML = "";
+            const movieList = document.createElement('ul');
+            movieList.className = 'movie_list';
 
-        data.movies.forEach(movie => {
-            const li = document.createElement('li');
-            li.textContent = movie;
-            movieListEl.appendChild(li);
+            cinema.movies.forEach(movie => {
+                const li = document.createElement('li');
+                li.textContent = movie;
+                movieList.appendChild(li);
+            });
+
+            cinemasContainerEl.appendChild(movieList);
         });
     }
     catch(error){
         console.error('映画のデータの取得に失敗しました:', error);
-        document.getElementById('cinema_name').textContent = 'データの取得に失敗しました';
+        statusMessageEl.style.display = "block";
+        statusMessageEl.textContent = "データの読み込みに失敗しました ";
     }
 }
 
